9368384893ad2f254cee46401488dfb7e7819f10
Shaurya Gupta <shaurya50211@gmail.com>
2023-10-07 13:56:19 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+var baseUrl = "https://jsonplaceholder.typicode.com"
+// https://jsonplaceholder.typicode.com/albums/1/photos
+
+const form = document.querySelector("form")
+form.addEventListener('submit', onSubmit)
+
+function onSubmit(e) {
+    e.preventDefault();
+    const albumID = e.target['albumID'].value
+    getPhotos(albumID)
+}
+
+function getPhotos(albumID) {
+    let url = `${baseUrl}/albums/${albumID}/photos`
+    fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return response.json()
+            } else {
+                throw new Error("There was an issue fetching images.");
+            }
+        })
+        .then(json => showImages(json))
+        .catch(err => alert(err))
+}
+
+function showImages(photos) {
+    const images = document.querySelector("#images")
+    images.innerHTML = ""
+    photos.forEach(photo => {
+        const img = document.createElement("img")
+        img.src = photo.url
+        images.appendChild(img)
+        // TODO: Add Caption
+    })
+}
